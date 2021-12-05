798fc387bf92d30cc35f49319306102f519f7afc
root <root@KBP1-LHP-A02596.synapse.com>
2021-12-06 00:50:08 +0200

    add tasks 1,2

---
+++ task-2.js
@@ -0,0 +1,8 @@
+let total = 100;
+let ordered = 100.0;
+if (ordered > 0 && ordered <= total) {
+    console.log("Замовлення оформлено, з Вами зв'яжеться менеджер!");
+}
+else {
+    console.log("На складі недостатньо товару!");
+}
